c3e38764f9c0d66fbd201fa10779cffb91f45301
Yampolskyi93 <leonid.yampolskyi@gmail.com>
2021-03-23 14:21:17 +0200

    Added one more case

--- unittests.py
+++ unittests.py
@@ -5,13 +5,16 @@ from test_work import test_work
 
 class GetTwoMaxTest(unittest.TestCase):
     def test_full_filled_list(self):
-        self.assertSequenceEqual(test_work.get_2_max([32, 11, 0 - 107, 4, 15]), (32, 15))
+        self.assertSequenceEqual(test_work.get_2_max([32, 11, 0, -107, 4, 15]), (32, 15))
 
     def test_list_with_one_element(self):
         self.assertSequenceEqual(test_work.get_2_max([32]), (32, None))
 
     def test_empty_list(self):
         self.assertSequenceEqual(test_work.get_2_max([]), (None, None))
+
+    def test_list_with_equal_elements(self):
+        self.assertSequenceEqual(test_work.get_2_max([1, 1, 1, 1, 1, 1]), (1, 1))
 
 
 if __name__ == '__main__':
