bad45028e82d0f0bbf5cc304861c58a1f5a634df
Yampolskyi93 <leonid.yampolskyi@gmail.com>
2021-03-24 21:42:07 +0200

    Added pytest

--- suite_unitests_for_two_max_digits.py
+++ suite_unitests_for_two_max_digits.py
@@ -1,9 +1,10 @@
 import unittest
+import pytest
 
 from test_work import test_work
 
 
-class GetTwoMaxTest(unittest.TestCase):
+class TestGetTwoMax(unittest.TestCase):
     def test_full_filled_list(self):
         self.assertSequenceEqual(test_work.get_2_max([32, 11, 0, -107, 4, 15]), (32, 15))
 
@@ -15,7 +16,3 @@ class GetTwoMaxTest(unittest.TestCase):
 
     def test_list_with_equal_elements(self):
         self.assertSequenceEqual(test_work.get_2_max([1, 1, 1, 1, 1, 1]), (1, 1))
-
-
-if __name__ == '__main__':
-    unittest.main()
